3b92f20053c98ba62b9e3d549e6f39765061c935
AbdouMurad <abdoumurad1522@gmail.com>
2024-09-11 23:45:59 -0600

    Add files via upload

---
+++ Asteroids/src/config.py
@@ -0,0 +1,12 @@
+from const import *
+import random
+
+class Config:
+    def __init__(self):
+        self.coord = []
+        self.create_stars()
+    def create_stars(self):
+        for x in range(int(WIDTH/15 +1)):
+            for y in range(int(HEIGHT/15 +1)):
+                self.coord.append((x*15 + random.randint(-8, 8), y*15 + random.randint(-8,8), random.randint(0, 255)))           
+        return self.coord
